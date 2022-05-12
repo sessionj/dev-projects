3f9f32c7cd5b0f82decd81e34bb0770cd1ff56d9
sessionj <_session@kakao.com>
2022-05-12 17:48:15 +0900

    202205111800

---
+++ mdaesin/app/src/main/java/kr/co/mdaesin/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package kr.co.mdaesin.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.co.mdaesin.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
