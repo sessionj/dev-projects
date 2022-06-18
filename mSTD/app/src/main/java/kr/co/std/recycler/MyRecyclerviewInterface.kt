06812010b18c69c2c960990938f27f49b8752151
sessionj <_session@kakao.com>
2022-06-18 11:43:29 +0900

    20220617

---
+++ mSTD/app/src/main/java/kr/co/std/recycler/MyRecyclerviewInterface.kt
@@ -0,0 +1,12 @@
+package kr.co.std.recycler
+
+/**
+ * @authar shadow
+ * @email _session@kakao.com
+ * @created 2022-06-18
+ * @desc
+ */
+
+interface MyRecyclerviewInterface {
+    fun onItemClicked(position: Int)
+}
